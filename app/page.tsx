1294bcf8d810a75dbeb2bd3186d8e364be70cf46
monicabaggio <monica.dbaggio@gmail.com>
2026-05-28 00:07:40 +0000

    configuracoes basicas

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+
+    <>
+    <h1> Bem vindo!</h1>
+    <Button variant = "secondary"> Salvar</Button>
+     </>
+  );
+
+}
